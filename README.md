9d203455d69cbf1e94d06b75fde3cad70afa3568
Paul Schwarzenberger <paul@celidor.net>
2019-11-12 12:01:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Azure Web App
+
+This is a fork from the 
+[Microsoft Azure Samples Web App](https://github.com/Azure-Samples/app-service-web-dotnet-get-started.git).
+
+## License
+
+See [LICENSE](LICENSE).
---
+++ aspnet-get-started/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnet_get_started.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A Cloud Guru.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hello Cloud Gurus. Let's get in touch ...";
+
+            return View();
+        }
+    }
+}
